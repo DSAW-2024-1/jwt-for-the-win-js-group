c3a344f41767619062116f86b0ddafa4a75d1965
Josu-s12 <josuedavidsarmiento@hotmail.com>
2024-05-15 21:45:45 -0500

    cosiato

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+function createToken(payload) {
+    return jwt.sign(payload, 'key_secret', { expiresIn: '1h' });
+}
+
+function verifyToken(req, res, next) {
+    const token = req.cookies.authToken;
+    if (!token) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    jwt.verify(token, 'key_secret', (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+        req.userData = decoded;
+        next();
+    });
+}
+
+const protectedRoutes = express.Router();
+
+protectedRoutes.use(verifyToken);
+
+protectedRoutes.get('/profile', (req, res) => {
+    const profileInfo = {
+        firstName: "Gaius",
+        lastName: "Marcellus",
+        email: "gaius.marcellus@example.com",
+        birthDate: "1985-07-12"
+
+    };
+    res.status(200).json(profileInfo);
+});
+
+protectedRoutes.get('/contacts', (req, res) => {
+    const contactsList = ['Gaius', 'Cornelia', 'Quintus', 'Livia', 'Tiberius', 'Octavia', 'Claudius', 'Valeria', 'Cicero', 'Antonia'];
+    res.status(200).json(contactsList);
+});
+
+protectedRoutes.post('/form', (req, res) => {
+    const { content } = req.body;
+    res.status(200).json({ content });
+});
+
+app.post('/signin', (req, res) => {
+    const { username, password } = req.body;
+    if (username === 'admin' && password === 'admin') {
+        const token = createToken({ username });
+        res.cookie('authToken', token, { httpOnly: true });
+        res.status(200).json({ message: 'Inicio de sesión correcto' });
+    } else {
+        res.status(401).json({ error: 'datos de sesion inválidas' });
+    }
+});
+
+app.use('/', protectedRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`El servidor funcionando y escuchando en el puerto ${PORT}`);
+});
